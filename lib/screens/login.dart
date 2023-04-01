60ec2e33546d3bb26f2163b751b0d68390a6fed1
Anas <anaskakkuwala123@gmail.com>
2023-04-01 16:21:22 +0530

    loginScreen Commit

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: Column(
-        children: [
-
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(height: deviceHeight * 0.30,
+            color: Colors.cyan,
+            ),
+          ],
+        ),
       ),
     );
   }
