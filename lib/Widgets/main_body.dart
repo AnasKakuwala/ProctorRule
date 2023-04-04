a685cd4d24d066d6a29171e52d63bdefc882bf58
Anas <anaskakkuwala123@gmail.com>
2023-04-05 03:10:41 +0530

    5/4/23 3:10am instituteScreen

---
+++ lib/Widgets/main_body.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:proctor_rule/screens/instituteSelection.dart';
+
+class MainBody extends StatefulWidget {
+  const MainBody({Key? key}) : super(key: key);
+
+  @override
+  State<MainBody> createState() => _MainBodyState();
+}
+
+class _MainBodyState extends State<MainBody> {
+
+  List<Widget> buttons = [];
+  var checkClick = false;
+  String textMsg = "";
+  void change(){
+    setState(() {
+      checkClick = !checkClick;
+      print(buttons.length);
+      if(buttons.length < 2){
+        textMsg = "Select the exam";
+        print("Button add");
+        if(checkClick){
+          buttons.add(
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+              primary: Colors.grey[700],
+            ),child: Text("ESE"),onPressed: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> InstituteSelection()));
+            },
+            ),
+          );
+          buttons.add(
+            ElevatedButton(child: Text("MSE"),onPressed: () => {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey[700],
+              ),),
+
+          );
+        }
+      }
+      else{
+        print("Button removed");
+        textMsg = "";
+        buttons.clear();
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 120,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/images/classroom.jpg'),fit: BoxFit.fill)
+          ),
+        ),
+        SizedBox(height: 10,),
+        ListTile(
+          shape: Border(
+            top: BorderSide(color: Colors.blue),
+            left: BorderSide(color: Colors.blue),
+            bottom: BorderSide(color: Colors.blue),
+            right: BorderSide(color: Colors.blue),
+          ),
+          leading: Icon(Icons.schedule),
+          title: Text("Schedule exam"),
+          onTap: () => {
+            change(),
+          },
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(textMsg),
+        SizedBox(
+          height: 20,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: buttons
+        ),
+      ],
+    );
+  }
+}
+
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import '../Widgets/app_drawer.dart';
+import '../Widgets/main_body.dart';
 
 
 class MainScreen extends StatelessWidget {
@@ -12,65 +13,33 @@ class MainScreen extends StatelessWidget {
       drawer: AppDrawer(),
       appBar: AppBar(
         title: Text("ProctorRule"),
+        backgroundColor: Colors.grey[700],
       ),
+
       // body: Padding(
-      //   padding: const EdgeInsets.all(5.0),
+      //   padding: const EdgeInsets.all(2.0),
       //   child: ListView(
       //     children: [
-      //       ListTile(
-      //
-      //         // shape: BoxBorder.lerp(Border(), Border.all(color: Colors.black), 2),
-      //         shape: Border.all(color: Colors.black,style: BorderStyle.solid,),
-      //         leading: Icon(Icons.schedule,color: Colors.red),
-      //         title: Text("Schedule Exam"),
-      //
+      //       InkWell(
       //         onTap: (){},
+      //         child: Card(
+      //           shape: BoxBorder.lerp(Border(), Border.all(color: Colors.red,), 2),
+      //           child: Container(
+      //             height: 50,
+      //             child: Row(
+      //               children: [
+      //                 Container(child: Icon(Icons.schedule,size: 50,),color: Colors.red),
+      //                 SizedBox(width: 20,),
+      //                 Text('Schedule Exam',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
+      //               ],
+      //             ),
+      //           ),
+      //         ),
       //       ),
       //     ],
       //   ),
       // ),
-      // body: Padding(
-      //     padding: EdgeInsets.all(8),
-      //     child: ListTile(
-      //       shape: Border.lerp(Border(),Border.all(), 2),
-      //       leading: Row(
-      //         mainAxisAlignment: MainAxisAlignment.start,
-      //         children: [
-      //           Container(
-      //             height: double.infinity,
-      //             width: 50,
-      //             decoration: BoxDecoration(color: Colors.red),
-      //             child:Icon(Icons.schedule)
-      //           ),
-      //         ],
-      //       ),
-      //       title: Text("Schedule Exam",style: TextStyle(fontSize: 20),),
-      //       onTap: (){},
-      //     ),
-      // ),
-      body: Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: ListView(
-          children: [
-            InkWell(
-              onTap: (){},
-              child: Card(
-                shape: BoxBorder.lerp(Border(), Border.all(color: Colors.red,), 2),
-                child: Container(
-                  height: 50,
-                  child: Row(
-                    children: [
-                      Container(child: Icon(Icons.schedule,size: 50,),color: Colors.red),
-                      SizedBox(width: 20,),
-                      Text('Schedule Exam',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: MainBody(),
     );
   }
 }
